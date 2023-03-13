5688b542e92b3baa80e2a1aadcec48ffcf2f5489
Mariana Doldan <marianamilanid@gmail.com>
2023-03-12 22:15:04 -0300

    comment correction

--- app.rb
+++ app.rb
@@ -1,22 +1,22 @@
 def choose(weight)
-  # This is a standard weighted randomization method, is is used to indicate the probability of a specific outcome.
+  # This is a standard weighted randomization method, it is used to indicate the probability of a specific outcome.
   # We will use it later to define the chances of a successful ride match, and the chances of churn.
   chance = rand()
   chance <= weight / 100.0
 end
 
 def ride_request(match, lyft_take)
-  # This method defines that every time a ride is requested, if there is a match, we should add the Lyft take rate to the yearly income (sum).
+  # This method defines that every time a ride is requested, if there is a match, we should add the Lyft take rate to the yearly revenue (sum).
   # The probability of a successful match is indicated in our loop below.
   if match == true
-    $yearly_income += lyft_take
+    $yearly_revenue += lyft_take
   end
 end
 
 
 def lose_rider(match, rider_churn)
   # This method defines riders' churn. Specifically, a 10% chance of losing a rider if a match is successful, and a 33% chance in case of no match.
-  # I'm calling an 'acquire_new_rider' method every time we lose a rider, assuming that we want to keep a stable operation, and won't allow the reduction of my customer base.
+  # I'm calling an 'acquire_new_rider' method every time we lose a rider, assuming that we want to keep a stable operation, and won't allow the reduction of our customer base.
   if match == true
     rider_churn = choose(10)
   else
@@ -27,12 +27,16 @@ def lose_rider(match, rider_churn)
 end
 
 def acquire_new_rider()
+  # For simplification, I'm considering acquiring a new rider for each one we lose, and disregarding a growth scenario on purpose.
     $rider_churn_count % 100 == 0 && $cac < 20 ? $cac += 0.5 : 0
     $invested_budget += $cac
 end
 
 def run(monthly_riders, rider_churn, lyft_take)
-  $yearly_income = 0
+  # This is the loop that will run 12 times, to simulate a 12-month period.
+  # I've established an 'if' condition that sets a 60% chance of a successful match in case our take rate is set at $6, and 93% in case our take rate is set at $3.
+  # All variables are initialized at 0 value, except for CAC, since the smallest possible CAC we know is $10.
+  $yearly_revenue = 0
   $rider_churn_count = 0
   $invested_budget = 0
   $cac = 10
@@ -50,13 +54,12 @@ def run(monthly_riders, rider_churn, lyft_take)
     end
   end
 
-  gross_profit = $yearly_income - $invested_budget
+  net_revenue = $yearly_revenue - $invested_budget
 
-  return {yearly_income: $yearly_income, invested_budget: $invested_budget, gross_profit: gross_profit, rider_churn_count: $rider_churn_count}
-  # puts "Yearly income: #{$yearly_income} | Invested budget: #{$invested_budget} | Gross profit: #{gross_profit} | Rider churn count: #{$rider_churn_count}"
+  return {yearly_revenue: $yearly_revenue, invested_budget: $invested_budget, net_revenue: net_revenue, rider_churn_count: $rider_churn_count}
 end
 
-lyft_take = 6
+lyft_take = 3
 # This is where we set our take.
 
 monthly_riders = 1000
@@ -66,30 +69,33 @@ monthly_drivers = 100
 
 rider_churn = false
 match = false
-# Initialized this variable at 'false', and will add later on the methods the chances of it becoming 'true' based on the match rate at each price.
+# Initialized this variable at 'false', since we added on the methods above the chances of it becoming 'true' based on the match rate at each price.
+# This is the end of the main model.
 
 results = []
+# Now that our model works, I'll just add a loop to run in n times and store results in a list, then calculate the average for a reliable estimation.
 
 100.times do
   results_hash = run(monthly_riders, rider_churn, lyft_take)
   results.append(results_hash)
 end
+# The larger the number of times, the more statistically significant our estimation gets.
 
-sum_gross_profit = 0
+sum_net_revenue = 0
 sum_invested_budget = 0
-sum_yearly_income = 0
+sum_yearly_revenue = 0
 sum_rider_churn_count = 0
 
 results.each do | result |
-  sum_yearly_income += result[:yearly_income]
+  sum_yearly_revenue += result[:yearly_revenue]
   sum_invested_budget += result[:invested_budget]
-  sum_gross_profit += result[:gross_profit]
+  sum_net_revenue += result[:net_revenue]
   sum_rider_churn_count += result[:rider_churn_count]
 end
 
-average_gross_profit = sum_gross_profit / results.size
-average_yearly_income = sum_yearly_income / results.size
+average_net_revenue = sum_net_revenue / results.size
+average_yearly_revenue = sum_yearly_revenue / results.size
 average_invested_budget = sum_invested_budget / results.size
 average_rider_churn_count = sum_rider_churn_count / results.size
 
-puts "Estimated yearly income: #{average_yearly_income.round} | Estimated invested budget: #{average_invested_budget.round} | Estimated gross profit: #{average_gross_profit.round} | Estimated rider churn count: #{average_rider_churn_count.round}"
+puts "Estimated yearly revenue: #{average_yearly_revenue.round} | Estimated invested budget: #{average_invested_budget.round} | Estimated net revenue: #{average_net_revenue.round} | Estimated rider churn count: #{average_rider_churn_count.round}"
